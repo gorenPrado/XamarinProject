83b350e4c8345de13fa454ba855532c2cff72a4f
Goren Prado <goren.p@hotmail.com>
2020-05-31 12:09:59 +0200

    dependencias

--- XamarinCalendarProject/XamarinCalendarProject/XamarinCalendarProject/App.xaml.cs
+++ XamarinCalendarProject/XamarinCalendarProject/XamarinCalendarProject/App.xaml.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinCalendarProject.Services;
 
 namespace XamarinCalendarProject
 {
     public partial class App : Application
     {
+        private static ServiceDependency _Locator;
+        public static ServiceDependency Locator
+        {
+            get
+            {
+                return _Locator = _Locator ?? new ServiceDependency();
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ XamarinCalendarProject/XamarinCalendarProject/XamarinCalendarProject/Services/ServicesDependency.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinCalendarProject.Repositorio;
+
+namespace XamarinCalendarProject.Services
+{
+    public class ServiceDependency
+    {
+
+        private IContainer container;
+
+        public ServiceDependency()
+        {
+            RegisterDependencies();
+        }
+
+        private void RegisterDependencies()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            
+            builder.RegisterType<RepositorioCalendario>();
+
+            container = builder.Build();
+        }
+
+    }
+}
