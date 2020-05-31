56c8de4afeb66cd536f9a29f804089badc5f5bc2
Goren Prado <goren.p@hotmail.com>
2020-05-31 11:51:39 +0200

    Eventos

---
+++ XamarinCalendarProject/XamarinCalendarProject/XamarinCalendarProject/Models/Eventos.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinCalendarProject.Models
+{
+    public class Eventos
+    {
+        [JsonProperty("idEvento")]
+        public int IdEvento { get; set; }
+        [JsonProperty("fechaInc")]
+        public DateTime FechaInc { get; set; }
+        [JsonProperty("fechaFnl")]
+        public DateTime FechaFnl { get; set; }
+        [JsonProperty("descripcion")]
+        public String Descripcion { get; set; }
+        [JsonProperty("idUsuario")]
+        public int IdUsuario { get; set; }
+    }
+}
