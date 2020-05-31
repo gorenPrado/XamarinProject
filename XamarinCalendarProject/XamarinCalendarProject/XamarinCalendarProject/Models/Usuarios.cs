45c0e4b0cec8158b29ea3378829329c8974a4c04
Goren Prado <goren.p@hotmail.com>
2020-05-31 11:52:39 +0200

    Usuarios

---
+++ XamarinCalendarProject/XamarinCalendarProject/XamarinCalendarProject/Models/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinCalendarProject.Models
+{
+    public class Usuarios
+    {
+        [JsonProperty("idUsuario")]
+        public int IdUsuario { get; set; }
+        [JsonProperty("nombre")]
+        public String Nombre { get; set; }
+        [JsonProperty("oficio")]
+        public String Oficio { get; set; }
+        [JsonProperty("nacionalidad")]
+        public String Nacionalidad { get; set; }
+        [JsonProperty("correo")]
+        public String Correo { get; set; }
+    }
+}
